925583456d7c597b887c0675bd762ad037c5a6f0
João Sarmento <joaovicentesarm@gmail.com>
2022-03-14 16:13:28 -0300

    Adição de Video

--- src/Posts.js
+++ src/Posts.js
@@ -1,6 +1,7 @@
 const posts = [
-    {userImg: 'assets/img/meowed.svg', user: 'meowed', imgPost: 'assets/img/gato-telefone.svg', userImgLiked: 'assets/img/respondeai.svg', userLiked: 'respondeai', likeNumber: '101.523'},
-    {userImg: 'assets/img/barked.svg', user: 'barked', imgPost: 'assets/img/dog.svg', userImgLiked: 'assets/img/adorable_animals.svg', userLiked: 'adorable_animals', likeNumber: '99.159'},
+    {userImg: 'assets/img/meowed.svg', user: 'meowed', imgPost: 'assets/img/gato-telefone.svg', userImgLiked: 'assets/img/respondeai.svg', userLiked: 'respondeai', likeNumber: '101.523', type: 'img'},
+    {userImg: 'assets/img/barked.svg', user: 'barked', imgPost: 'assets/img/dog.svg', userImgLiked: 'assets/img/adorable_animals.svg', userLiked: 'adorable_animals', likeNumber: '99.159', type: 'img'},
+    {userImg: 'assets/img/ursodalagoinha.jpeg', user: 'ursodalagoinha', imgPost: 'assets/video/video.mp4', userImgLiked: 'assets/img/sansaoboy.png', userLiked: 'sansaoboy', likeNumber: '1.435.343', type: 'video', oggPost: 'assets/video/video.ogv'}
 ]
 
 export default function Posts(){
@@ -12,7 +13,7 @@ export default function Posts(){
 }
 
 function Post(props){
-    const {userImg, user, imgPost, userImgLiked, userLiked, likeNumber} = props;
+    const {userImg, user, imgPost, userImgLiked, userLiked, likeNumber, type, oggPost} = props;
     return(
         <div className="post">
             <div className="topo">
@@ -26,7 +27,7 @@ function Post(props){
               </div>
 
               <div className="conteudo">
-                <img src={imgPost} />
+                <Conteudo imgPost = {imgPost} type = {type} oggPost = {oggPost}/>
               </div>
 
               <div className="fundo">
@@ -42,7 +43,9 @@ function Post(props){
                 </div>
 
                 <div className="curtidas">
+                  <div className='usuario'>
                   <img src={userImgLiked} />
+                  </div>
                   <div className="texto">
                     Curtido por <strong>{userLiked}</strong> e <strong>outras {likeNumber} pessoas</strong>
                   </div>
@@ -50,4 +53,20 @@ function Post(props){
             </div>
         </div>
     )
+}
+
+function Conteudo ({imgPost, type, oggPost}){
+  if(type == 'img'){
+  return(
+      <img src={imgPost} />
+  )
+  }else{
+    return (
+      <video autoPlay muted>
+        <source src={imgPost} type="video/mp4"/>
+        <source src={oggPost} type="video/ogg"/>
+        Your browser does not support the video tag.
+      </video>
+    )
+  }
 }
